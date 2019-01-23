491203f126d2052ea2ef08546a44683df61753fb
Ashley Davis <ashley@codecapers.com.au>
2019-01-23 15:11:37 +1000

    Exported the function optimizeSingleParameter.

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ export { IAnalysis } from './lib/analysis';
 export { backtest } from './lib/backtest';
 export { analyze } from './lib/analyze';
 export { monteCarlo } from './lib/monte-carlo';
-export { optimize, OptimizeSearchDirection } from './lib/optimize';
+export { optimize, optimizeSingleParameter, OptimizeSearchDirection } from './lib/optimize';
 export { walkForwardOptimize } from './lib/walk-forward-optimize';
 export { computeEquityCurve } from './lib/compute-equity-curve';
 export { computeDrawdown } from './lib/compute-drawdown';
